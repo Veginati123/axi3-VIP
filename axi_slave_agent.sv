23faf39b8f1e9bc8eca26e8b5b5d074273096d2f
Veginati123 <veginatimounika245@gmail.com>
2025-10-02 14:36:26 +0530

    Update axi_slave_agent.sv

--- axi_slave_agent.sv
+++ axi_slave_agent.sv
@@ -1,1 +1,13 @@
 
+class axi_slave_agent extends uvm_agent;
+	//factory registration
+	`uvm_component_utils(axi_slave_agent)
+	function new(string name="",uvm_component parent=null);
+		super.new(name,parent);
+	endfunction
+	axi_slave_driver sdrv;
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		sdrv=axi_slave_driver::type_id::create("sdrv",this);
+	endfunction
+endclass
