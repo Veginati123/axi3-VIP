890103cb4b4b7850cdee065d2dda21a46a71e87f
Veginati123 <veginatimounika245@gmail.com>
2025-10-02 14:38:02 +0530

    Update axi_test.sv

--- axi_test.sv
+++ axi_test.sv
@@ -1,1 +1,31 @@
+class axi_test extends uvm_test;
+	//factory registration
+	axi_base_sequence seq;
+	axi_top_env top_env;
 
+	`uvm_component_utils(axi_test)
+	//memeory allcoation
+	function new(string name="axi_test",uvm_component parent=null);
+		super.new(name,parent);
+	endfunction
+		//axi_base_sequence seq;
+
+	//we are including env
+	//axi_top_env top_env;//handle creation
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+		//object creation
+	seq=axi_base_sequence::type_id::create("seq",this);
+	top_env=axi_top_env::type_id::create("top_env",this);
+	endfunction
+	task run_phase(uvm_phase phase);
+		super.run_phase(phase);
+		// we need to connect sequence to sequncer
+		//connection sequence to sqr by using start method
+		// objects creation of sequnce
+		phase.raise_objection(this);
+		seq.start(top_env.menv.agent.seqr);
+		phase.drop_objection(this);
+		
+	endtask
+endclass
